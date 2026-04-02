21f291989d879ade66ead189d09ba61259d294f8
atulantonyz <40585728+atulantonyz@users.noreply.github.com>
2026-04-01 22:29:03 -0400

    added function to convert text node to html node

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from textnode import TextNode, TextType
+from htmlnode import LeafNode
+
 def main():
     new_text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(new_text_node)
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -34,6 +34,28 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is some anchor text")
+        self.assertEqual(html_node.props,{"href":"https://www.boot.dev"})
+
+    def test_image(self):
+        node = TextNode("This is some alt text", TextType.IMAGE, "https://www.boot.dev/boots.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props,{"src":"https://www.boot.dev/boots.png","alt":"This is some alt text"})
+
 
 
 if __name__ == "__main__":
