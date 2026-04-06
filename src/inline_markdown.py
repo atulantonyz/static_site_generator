be4762cca9eb73bba5b9fe8744efa7afb9596c35
atulantonyz <40585728+atulantonyz@users.noreply.github.com>
2026-04-05 21:13:49 -0400

    added function to convert inline markdown text to list of text nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -57,3 +57,13 @@ def split_nodes_link(old_nodes):
                 new_nodes.append(TextNode(node_links[(i-1)//2][0],TextType.LINK, node_links[(i-1)//2][1]))
     return new_nodes
 
+def text_to_textnode(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes,"**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes,"_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes,"`",TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
