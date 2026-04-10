0218b55cd41764fb0893c0589acd4182665a986e
atulantonyz <40585728+atulantonyz@users.noreply.github.com>
2026-04-10 00:18:45 -0400

    Added function to copy contents from static dir to public dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
-from textnode import TextNode, TextType
-from htmlnode import LeafNode
+import shutil
+import os
 
 def main():
-    new_text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(new_text_node)
+    SRC_DIR = "./static"
+    DST_DIR = "./public"
+    print("Deleting public directory...")
+    if os.path.exists(DST_DIR):
+        shutil.rmtree(DST_DIR)
+    print("Copying static content to public directory...")
+    copy_content(SRC_DIR, DST_DIR)
+
+def copy_content(src_dir, dst_dir):
+    if not os.path.exists(dst_dir):
+        os.mkdir(dst_dir)
+    children = os.listdir(src_dir)
+    for child_name in children:
+        child_path = os.path.join(src_dir, child_name)
+        dst_child_path = os.path.join(dst_dir, child_name)
+        print(f" * {child_path} -> {dst_child_path}")
+        if os.path.isfile(child_path):
+            shutil.copy(child_path, dst_dir)
+        else:
+            copy_content(child_path, dst_child_path)
+
 
 
 if __name__ == "__main__":
